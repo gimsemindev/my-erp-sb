b8dc63371fea920ab84f62fc11694cbd3aca71a9
gimsemindev <gimsemindev@gmail.com>
2026-03-31 17:07:21 +0900

    Commit Message

---
+++ src/test/java/com/sp/app/MyErpSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyErpSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
